2b8efd5e66877b1e5be146225a184ebe66148f4a
Lauren Hess <laurenbethhess@gmail.com>
2021-11-04 12:06:37 -0400

    commit tree c901d97ab813447cfce5b6886da409277ab5debc

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Add your code here
+
+
+//     function submitData(name, email) {
+//         fetch("http://localhost:3000/users", {
+//             method: "POST",
+//             headers: {
+//               "Content-Type": "application/json",
+//               Accept: "application/json",
+//             },
+//             body: JSON.stringify({
+//                 name: `${name}`,
+//                 email: `${email}`,
+//             }),
+//         })
+//         .then(resp => resp.json())
+//         .then(data => {
+//             let newh1 = document.createElement('h1')
+//             document.body.append(newh1)
+//             newh1.innerhtml = data.id
+//         })
+//         .catch(error => {
+//             let newError = document.createElement('h1')
+//             newError.innerHTML = error.message
+//             document.body.append(newError)
+//           })
+//     }
+
+// submitData('Lauren', 'laurenbethhess@gmail.com')
+
+
+function submitData(name, email) {
+    const configurationObjects = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`,
+        })
+    }       
+    return fetch("http://localhost:3000/users", configurationObjects)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        let newh1 = document.createElement('h1')
+        newh1.innerHTML = object.id;
+        document.body.append(newh1)
+    })
+    .catch(function (error) {
+        let newError = document.createElement('h1')
+        newError.innerHTML = error.message
+        document.body.append(newError)
+      });
+} 
